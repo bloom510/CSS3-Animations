eb32246b557d59737a12c79823deac300aa11102
wildcat510 <bloomjazzer@gmail.com>
2018-03-20 14:14:24 -0700

    Initial commit

---
+++ logic.js
@@ -0,0 +1,52 @@
+/*
+
+*/
+
+$(document).ready(function() {
+    clicked = $('.one');
+    let right = [];
+    let left = [];
+    let prev_page = 'home';
+    $('nav li').click(function() {
+        //remove current class and assign new
+        if (clicked) clicked.removeClass('current');
+        clicked = $(this);
+        $(this).addClass('current');
+
+        //get current position of link and move the slider
+        let current_pos = ($('nav').width() / 6.8) * $('.current').index();
+        $('.slider').css('margin-left', `${current_pos}px`)
+
+        //CSS animations
+        let page = $(this).attr('page');
+        if (right.includes(page)) {
+            $(`#${prev_page}`).css('animation', 'hide-left 2s forwards').fadeOut(750)
+
+            setTimeout(function() {
+                $(`#${page}`).show().css('animation', 'show-center-from-right 2s forwards')
+            }, 500)
+            right.splice(right.indexOf(page))
+        } else if (!right.includes(page)) {
+            right.push(prev_page)
+            setTimeout(function() {
+                $(`#${page}`).show().css('animation', 'show-center-from-left 2s forwards')
+
+            }, 500);
+            $(`#${prev_page}`).css('animation', 'hide-right 2s forwards').fadeOut(750)
+
+            // $(`#${page}`).show().css('animation', 'show-center-from-left 2s forwards')
+        }
+
+        prev_page = $(clicked).attr('page');
+        console.log(right)
+    })
+
+    $('nav li ').hover(function() {
+        let current_pos = ($('nav').width() / 6.8) * $(this).index();
+        $('.slider').css('margin-left', `${current_pos}px`)
+    })
+    $('nav li ').mouseleave(function() {
+        let current_pos = ($('nav').width() / 6.8) * $('.current').index();
+        $('.slider').css('margin-left', `${current_pos}px`)
+    })
+})
