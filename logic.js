ed707cbd4b5f0ef25f10722e62c07253364e782f
wildcat510 <bloomjazzer@gmail.com>
2018-03-21 16:17:48 -0700

    testing out cookies js

--- logic.js
+++ logic.js
@@ -22,7 +22,7 @@ $(document).ready(function() {
         let current_pos = ($('nav').width() / 6.8) * $('.current').index();
         $('.slider').css('left', `${current_pos}px`)
             //animate slider and page
-        $(`#${prev_page}`).css('animation', 'hide-down 2s forwards').fadeOut(750)
+        $(`#home`).css('animation', 'hide-down 2s forwards').fadeOut(750)
         $(`#${page}`).fadeIn(1500).css('animation', 'show-center-from-bottom 2s forwards')
     }
     $('nav li').click(function() {
