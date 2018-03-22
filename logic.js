13a7586d6ff3886364288971e798a0445410d08d
wildcat510 <bloomjazzer@gmail.com>
2018-03-22 12:26:45 -0700

    removed cookie store for more preferable makeshift router

--- logic.js
+++ logic.js
@@ -1,30 +1,27 @@
 $(document).ready(function() {
-    //page memory contains cookies
-    let page_memory = document.cookie;
+    var currentURL = document.URL;
+    var params = currentURL.extract();
+
     //current page
     let page;
     //default value for clicked (helper for animated slider)
     clicked = $('.home');
     //stores previous page after click to a new page
     let prev_page;
-
-    if (page_memory) {
-        page = $.cookie('page'); //current page value
-        clicked = $(`.${page}`) //will behave as if clicked
-        prev_page = $.cookie('prev_page'); //previous page
-        //remove current class from home and/or prev page
+    if (params) {
+        page = params.page;
+        clicked = $(`.${page}`)
         $(`.home`).removeClass('current');
-        $(`.${prev_page}`).removeClass('current');
-        //add current class to the stored page
         $(`.${page}`).addClass('current');
         //animate slider
-        let current_pos = ($('nav').width() / 6.8) * $('.current').index();
-        $('.slider').css('margin-left', `${current_pos}px`);
+        // let current_pos = ($('nav').width() / 6.8) * $('.current').index();
+        // $('.slider').css('margin-left', `${current_pos}px`);
         //hide and show appropriate sections
         if (page !== 'home') $('#home').hide()
         $(`#${prev_page}`).css('animation', 'hide-down 2s forwards').fadeOut(750)
         $(`#${page}`).fadeIn(1500).css('animation', 'show-center-from-bottom 2s forwards')
     }
+
     //get current position of link and move the slider
     let current_pos = ($('nav').width() / 6.8) * $('.current').index();
     $('.slider').css('margin-left', `${current_pos}px`)
@@ -35,8 +32,7 @@ $(document).ready(function() {
             if (page) $(`.${page}`).removeClass('current');
             //turn old click into prev_page, so we know to hide it
             prev_page = clicked.attr('page');
-            //store prev_page as cookie
-            $.cookie('prev_page', prev_page);
+
             //remove current class from old click
             clicked.removeClass('current');
         }
@@ -51,7 +47,6 @@ $(document).ready(function() {
 
         //page is new click
         page = $(this).attr('page');
-        $.cookie('page', page)
 
         //if new page click
         if (page !== prev_page) {
