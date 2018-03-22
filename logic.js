7b450e90d4fb0398a94124c901d1328e6601268b
wildcat510 <bloomjazzer@gmail.com>
2018-03-21 18:55:40 -0700

    Functional cookie session, will make less DRY

--- logic.js
+++ logic.js
@@ -1,37 +1,37 @@
 $(document).ready(function() {
-
-
-    // $('.container').hide();
+    //page memory contains cookies
     let page_memory = document.cookie;
+    let current_pos = ($('nav').width() / 6.8) * $('.current').index();
+    //current page
     let page;
-
+    //default value for clicked (helper for animated slider)
     clicked = $('.home');
+    //stores previous page after click to a new page
     let prev_page;
 
     if (page_memory) {
-
-        page = $.cookie('page');
-        clicked = $(`.${page}`)
-        prev_page = $.cookie('prev_page');
+        page = $.cookie('page'); //current page value
+        clicked = $(`.${page}`) //will behave as if clicked
+        prev_page = $.cookie('prev_page'); //previous page
         //remove current class from home and/or prev page
         $(`.home`).removeClass('current');
         $(`.${prev_page}`).removeClass('current');
         //add current class to the stored page
         $(`.${page}`).addClass('current');
         //animate slider
-        let current_pos = ($('nav').width() / 6.8) * $('.current').index();
-        $('.slider').css('margin-left', `${current_pos}px`)
-            //hide and show appropriate sections
+
+        $('.slider').css('margin-left', `${current_pos}px`);
+        //hide and show appropriate sections
         if (page !== 'home') $('#home').hide()
         $(`#${prev_page}`).css('animation', 'hide-down 2s forwards').fadeOut(750)
         $(`#${page}`).fadeIn(1500).css('animation', 'show-center-from-bottom 2s forwards')
     }
     //get current position of link and move the slider
-    let current_pos = ($('nav').width() / 6.8) * $('.current').index();
+
     $('.slider').css('margin-left', `${current_pos}px`)
 
     $('nav li').click(function() {
-        //remove current class and assign new
+        //
         if (clicked) {
             if (page) $(`.${page}`).removeClass('current');
             //turn old click into prev_page, so we know to hide it
